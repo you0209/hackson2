56d185e7f078d5a4a3746d70e1d42a9946632afc
youchon <youchon20koudoukouya@gmail.com>
2026-06-17 12:56:35 +0900

    Add XSHUT pin reset for VL53L0X in hackson_test4

--- src/tantoB/hackson_test4/hackson_test4.ino
+++ src/tantoB/hackson_test4/hackson_test4.ino
@@ -7,6 +7,7 @@ VL53L0X sensor;
 // 共通変数
 #define PHOTO_PIN A1
 #define LED_PIN 3
+#define XSHUT_PIN 7
 unsigned long nextBeatTime = 0; //次の拍の予測絶対時刻(milli)
 unsigned long currentBeatTime = 0;
 bool beatUpdated = false; //nectBeatTime更新時にtrue
@@ -17,6 +18,13 @@ State state = IDLE;
 void setup() {
   Serial.begin(115200);
   Wire.begin();
+  // XSHUTでVL53L0Xをハードウェアリセット
+  pinMode(XSHUT_PIN, OUTPUT);
+  digitalWrite(XSHUT_PIN, LOW);
+  delay(10);
+  digitalWrite(XSHUT_PIN, HIGH);
+  delay(10);
+
   sensor.setTimeout(500);
   if (!sensor.init()) {
     Serial.println("VL53L0X init FAILED");
