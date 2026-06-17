dffbc819307786d441e45cb5ff32f2b9bdbfab26
kaira <kairazhangshan50@gmail.com>
2026-06-17 15:58:06 +0900

    更新(06/17)

--- src/tantoB/hackson_test5/C.ino
+++ src/tantoB/hackson_test5/C.ino
@@ -1,14 +1,46 @@
 int noteIndex = 0; //現在の音符ポインタ
-uint8_t sendTime = 0; //シリアル通信を行う時間
+unsigned long soundTime;
+int i=0;
+unsigned long sendTime = 0; //シリアル通信を行う時間
 uint8_t note_off = 1; //前の音の終了
 uint8_t note = 0; //音名インデックス（休符＝7）
 uint8_t vel = 0; //音の強さ（0~127）
-uint8_t note_long = 0; //音の長さ
+float note_long = 0; //音の長さ
 const uint8_t score[][3] = {
-  {0, 100,1},
-  {1, 100,1},
-  {2, 100,1}
-};
+    {0, 100, 1},//ド　
+    {1, 100, 1},//レ
+    {2, 100, 1},//ミ
+    {3, 100, 1},//ファ｜
+    {2, 100, 1},//ミ　
+    {1, 100, 1},//レ
+    {0, 100, 1},//ド｜
+    {2, 127, 1},//ミ
+    {3, 127, 1},//ファ
+    {4, 127, 1},//ソ
+    {5, 127, 1},//ラ｜
+    {4, 127, 1},//ソ
+    {3, 127, 1},//ファ
+    {2, 127, 1},//ミ｜
+    {0, 90,  1},//ド
+    {6, 100, 1},
+    {0, 90,  1},//ド｜
+    {6, 100, 1},
+    {0, 90,  1},//ド
+    {6, 100, 1},
+    {0, 90,  1},//ド｜
+    {6, 100, 1},
+    {0, 100, 1},//ド
+    {0, 100, 1},//ド
+    {1, 100, 1},//レ
+    {1, 100, 1},//レ
+    {2, 100, 1},//ミ
+    {2, 100, 1},//ミ
+    {3, 100, 1},//ファ
+    {3, 100, 1},//ファ｜
+    {2, 100, 1},//ミ
+    {1, 100, 1},//レ
+    {0, 100, 1}//ド
+  };
 const int SCORE_LEN = sizeof ( score ) / sizeof ( score[0]);
 
 unsigned long sendTime_set(unsigned long nextBeatTime) {//担当Bよりより出された次の拍の予測絶対時間に補正値をかける
@@ -30,15 +62,18 @@ unsigned long sendTime_set(unsigned long nextBeatTime) {//担当Bよりより出
 
 void C_loop() {
   if (state != IDLE && state != CALIBRATE && state != PREBEAT && state != FERMATA) {
-    if(noteIndex <= SCORE_LEN){
-      if (beatUpdated && isPlaying) {
-        sendTime = sendTime_set(nextBeatTime);
+    if(noteIndex < SCORE_LEN){
 
         note = score[noteIndex][0];
         vel  = score[noteIndex][1];
         note_long = score[noteIndex][2];
-        
-         if (millis() >= sendTime){
+      if (beatUpdated) {
+
+           if (note_long < 1.0){
+            soundTime = currentBeatTime + ( nextBeatTime - currentBeatTime ) * note_long;
+            sendTime = sendTime_set(soundTime);
+
+            if (millis() >= sendTime){
            Serial.write(note_off);
            Serial.write(note);
            Serial.write(vel);
@@ -46,8 +81,33 @@ void C_loop() {
 
            noteIndex++;
            beatUpdated = false;
+            }
 
         }
+           else {
+            if (i==0){
+           sendTime = sendTime_set(soundTime);
+            if (millis() >= sendTime){
+           Serial.write(note_off);
+           Serial.write(note);
+           Serial.write(vel);
+           Serial.write(note_long);
+
+           beatUpdated = false;
+           i++;
+            }           
+           }
+             else if (i < note_long){
+              sendTime = sendTime_set(soundTime);    
+              if (millis() >= sendTime){          
+              beatUpdated = false;
+              i++;
+              }
+             }
+             else {
+               noteIndex++;
+               i=0;
+               }
       
       }
       if (noteIndex == SCORE_LEN) noteIndex = 0;
@@ -55,3 +115,6 @@ void C_loop() {
   }
  
 }
+}
+
+
